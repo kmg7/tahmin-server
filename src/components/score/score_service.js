51fab01976d294bf26feff26f35a8890ccbeb6b3
kmlgkcy <gmkemalim@gmail.com>
2022-12-12 21:54:13 +0300

    score model refactor

--- src/components/score/score_service.js
+++ src/components/score/score_service.js
@@ -1,79 +1,143 @@
-const model = require('./score_model');
 const modelError = require('../../errors/model_error');
+const { dbModel, Models } = require('../../utils/database');
+const score = Models.SCORE;
 const Joi = require('joi');
 
 const searchScore = async (data) => {
   try {
-    await validateId(data.id);
-    const response = await model.searchScore(data.id);
-    return handleResponse(response);
+    await validate({ schema: scoreFindSchema, data: data, field: 'score' });
+    await validate({ schema: scoreSortSchema, data: data, field: 'sort' });
+    await validate({ schema: scoreSelectSchema, data: data, field: 'select' });
+    await validate({ schema: paginationSchema, data: data, field: 'pagination' });
+    return handleResponse(
+      await dbModel.getMany({
+        model: score,
+        where: { [data.score.where]: { startsWith: data.score.value } },
+        select: data.select,
+        orderBy: { [data.sort.field]: data.sort.order },
+        skip: data.pagination.skip,
+        take: data.pagination.take,
+      })
+    );
   } catch (error) {
     return handleError(error);
   }
 };
-const getAllScores = async () => {
+
+const getAllScores = async (data) => {
   try {
-    const response = await model.getAllScores();
-    return handleResponse(response);
+    await validate({ schema: scoreSortSchema, data: data, field: 'sort' });
+    await validate({ schema: scoreSelectSchema, data: data, field: 'select' });
+    await validate({ schema: paginationSchema, data: data, field: 'pagination' });
+    return handleResponse(
+      await dbModel.getMany({
+        model: score,
+        select: data.select,
+        orderBy: { [data.sort.field]: data.sort.order },
+        skip: data.pagination.skip,
+        take: data.pagination.take,
+      })
+    );
   } catch (error) {
     return handleError(error);
   }
 };
+
 const createScore = async (data) => {
   try {
-    await validate(data.score);
-    const response = await model.createScore(data.score);
-    return handleResponse(response);
+    await validate({ schema: scoreCreateSchema, data: data, field: 'score' });
+    await validate({ schema: scoreSelectSchema, data: data, field: 'select' });
+    return handleResponse(
+      await dbModel.create({
+        model: score,
+        select: data.select,
+        data: data.score,
+      })
+    );
   } catch (error) {
     return handleError(error);
   }
 };
+
 const getScore = async (data) => {
   try {
-    await validateId(data.id);
-    const response = await model.getScore(data.id);
-    return handleResponse(response);
+    await validate({ schema: scoreFindSchema, data: data, field: 'score' });
+    await validate({ schema: scoreSelectSchema, data: data, field: 'select' });
+    return handleResponse(
+      await dbModel.get({
+        model: score,
+        where: { [data.score.where]: data.score.value },
+        select: data.select,
+      })
+    );
   } catch (error) {
     return handleError(error);
   }
 };
+
 const updateScore = async (data) => {
   try {
-    await validateId(data.id);
-    await validate(data.score, false);
-    const response = await model.updateScore(data.id, data.score);
-    return handleResponse(response);
+    await validate({ schema: scoreFindSchema, data: data, field: 'score' });
+    await validate({ schema: scoreSelectSchema, data: data, field: 'select' });
+    await validate({ schema: scoreUpdateSchema, data: data, field: 'update' });
+    return handleResponse(
+      await dbModel.update({
+        model: score,
+        where: { [data.score.where]: data.score.value },
+        data: data.update,
+        select: data.select,
+      })
+    );
   } catch (error) {
     return handleError(error);
   }
 };
+
 const deleteScore = async (data) => {
   try {
-    await validateId(data.id);
-    const response = await model.deleteScore(data.id);
-    return handleResponse(response);
+    await validate({ schema: scoreFindSchema, data: data, field: 'score' });
+    await validate({ schema: scoreSelectSchema, data: data, field: 'select' });
+    return handleResponse(
+      await dbModel.remove({
+        model: score,
+        where: { [data.score.where]: data.score.value },
+        select: data.select,
+      })
+    );
   } catch (error) {
     return handleError(error);
   }
 };
+
 const deleteManyScore = async (data) => {
   try {
-    await validateIds(data.ids);
-    const response = await model.deleteManyScore(data.ids);
-    return handleResponse(response);
+    await validate({ schema: scoreFindManySchema, data: data, field: 'scores' });
+    return handleResponse(
+      await dbModel.removeMany({
+        model: score,
+        where: { [data.scores.where]: { in: data.scores.values } },
+      })
+    );
   } catch (error) {
     return handleError(error);
   }
 };
+
 const createManyScore = async (data) => {
   try {
-    await validateMany(data.scores);
-    const response = await model.createManyScore(data.scores);
-    return handleResponse(response);
+    await validate({ schema: scoreCreateManySchema, data: data, field: 'scores' });
+    return handleResponse(
+      await dbModel.createMany({
+        model: score,
+        data: data.scores,
+        select: data.select,
+      })
+    );
   } catch (error) {
     return handleError(error);
   }
 };
+
 const handleResponse = (response) => {
   if (!response.success) {
     return {
@@ -86,7 +150,14 @@ const handleResponse = (response) => {
     data: response.data,
   };
 };
+
 const handleError = (error) => {
+  if (error.isValidation) {
+    return {
+      success: false,
+      error: modelError.NOT_PROVIDED(error.meta),
+    };
+  }
   if (error.isJoi) {
     return {
       success: false,
@@ -97,25 +168,39 @@ const handleError = (error) => {
     success: false,
   };
 };
-const validate = async (tournament, iscreate = true) => {
-  if (iscreate) {
-    await scoreCreateSchema.validateAsync(tournament);
-  } else {
-    await scoreUpdateSchema.validateAsync(tournament);
+
+const validate = async ({ schema, data, field }) => {
+  if (!data[field]) {
+    throw { isValidation: true, meta: `${field}` };
   }
-};
-const validateMany = async (Score) => {
-  await scoreCreateManySchema.validateAsync(Score);
-};
-const validateId = async (id) => {
-  await idSchema.validateAsync(id);
-};
-const validateIds = async (ids) => {
-  await idArraySchema.validateAsync(ids);
+  await schema.validateAsync(data[field]);
 };
 
-const idSchema = Joi.string().min(2).max(64).required();
-const idArraySchema = Joi.array().items(idSchema).min(2).required();
+const paginationSchema = Joi.object({
+  skip: Joi.number().integer().min(0),
+  take: Joi.number().integer().min(5).max(100).required(),
+});
+
+const scoreSortSchema = Joi.object({
+  field: Joi.string().allow('username', 'id', 'standingsId').required(),
+  order: Joi.string().allow('asc', 'desc').required(),
+}).required();
+
+const scoreFindSchema = Joi.object({
+  where: Joi.string().allow('username', 'id', 'standingsId').required(),
+  value: Joi.string().min(2).required(),
+}).required();
+
+const scoreFindManySchema = Joi.object({
+  where: Joi.string().allow('username', 'id', 'standingsId').required(),
+  values: Joi.array().items(Joi.string().min(2)).min(1).required(),
+});
+
+const scoreSelectSchema = Joi.object({
+  id: Joi.boolean(),
+  username: Joi.boolean(),
+  standingsId: Joi.boolean(),
+}).required();
 
 const scoreCreateSchema = Joi.object({
   id: Joi.string(),
