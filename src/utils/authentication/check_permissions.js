13f9791600706419bdcb4389ccec78aa86f4ca3c
kmlgkcy <gmkemalim@gmail.com>
2023-01-16 00:36:10 +0300

    bypass permission check when env is development

--- src/utils/authentication/check_permissions.js
+++ src/utils/authentication/check_permissions.js
@@ -1,4 +1,5 @@
 const checkPermissions = ({ requestUser, resourceUserId }) => {
+  if (process.env.NODE_ENV == 'development') return true;
   if (process.env.SUPERUSERS.includes(requestUser.authId)) return true;
   if (resourceUserId) {
     if (requestUser.authId === resourceUserId) return true;
