5a3dd8ca563390cc791225250f88bfd27a8f26c5
kmlgkcy <gmkemalim@gmail.com>
2023-01-19 23:21:12 +0300

    controller utils

---
+++ src/utils/controller/controller_manager.js
@@ -0,0 +1,79 @@
+import { handleResponse } from './controller_utils.js';
+
+export class ControllerManager {
+  service;
+
+  constructor({ service }) {
+    this.service = service;
+  }
+
+  async create(req, res, fields) {
+    const response = await this.service.create({ data: req.body, select: fields });
+    handleResponse({ serviceRes: response, res: res, isCreate: true });
+  }
+
+  async createMany(req, res, fields) {
+    const response = await this.service.createMany({ data: req.body, select: fields });
+    handleResponse({ serviceRes: response, res: res, isCreate: true });
+  }
+
+  async get(req, res, fields) {
+    const { wh: field, val: value } = req.query;
+    const response = await this.service.get({ where: { field: field, value: value }, select: fields });
+    handleResponse({ serviceRes: response, res: res });
+  }
+
+  async getMany(req, res, fields, search) {
+    const { wh: field, cond: condition, val: value, sort: sortBy, order: order, skip: skip, take: take } = req.query;
+    let params = {
+      sort: { sortBy: sortBy ?? 'id', order: order ?? 'asc' },
+      pagination: { skip: skip ?? 0, take: take ?? 30 },
+      select: fields,
+    };
+    if (search) {
+      params.search = { field: field, condition: condition, value: value };
+    }
+    const response = await this.service.getMany(params);
+    handleResponse({ serviceRes: response, res: res });
+  }
+
+  async getProperty(req, res, propFields) {
+    const { prop: property, id: id } = req.params;
+    const { wh: field, cond: condition, val: value, sort: sortBy, order: order, skip: skip, take: take } = req.query;
+    let params = {
+      id: id,
+      property: property,
+      select: propFields,
+      sort: { sortBy: sortBy ?? 'id', order: order ?? 'asc' },
+      pagination: { skip: skip ?? 0, take: take ?? 30 },
+    };
+    if (field && condition && value) {
+      params.search = { field: field, condition: condition, value: value };
+    }
+    const response = await this.service.getProperty(params);
+    handleResponse({ serviceRes: response, res: res });
+  }
+
+  async update(req, res, fields) {
+    const { wh: field, val: value } = req.query;
+    const response = await this.service.update({ where: { field: field, value: value }, data: req.body, select: fields });
+    handleResponse({ serviceRes: response, res: res });
+  }
+
+  async upsert(req, res, fields) {
+    const response = await this.service.upsert({ data: req.body, select: fields });
+    handleResponse({ serviceRes: response, res: res });
+  }
+
+  async remove(req, res, fields) {
+    const { wh: field, val: value } = req.query;
+    const response = await this.service.remove({ where: { field: field, value: value }, select: fields });
+    handleResponse({ serviceRes: response, res: res });
+  }
+
+  async removeMany(req, res) {
+    const { wh: field, val: value } = req.query;
+    const response = await this.service.removeMany({ where: { field: field, value: value } });
+    handleResponse({ serviceRes: response, res: res });
+  }
+}
