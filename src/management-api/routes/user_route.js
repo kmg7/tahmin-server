6820d129fefb4a5fa0e28d64228a52aebd5ff33f
kmlgkcy <gmkemalim@gmail.com>
2023-01-21 23:50:06 +0300

    component: user

---
+++ src/management-api/routes/user_route.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+const router = Router();
+
+import {
+  createUser,
+  createManyUser,
+  updateUser,
+  deleteUser,
+  deleteManyUser,
+  getUser,
+  getAllUsers,
+  searchUser,
+  getUserProperty,
+} from '../controller/user_controller.js';
+
+router.route('/').get(getUser).post(createUser).put(updateUser).delete(deleteUser);
+router.route('/many').get(getAllUsers).post(createManyUser).put(upsertUser).delete(deleteManyUser);
+router.route('/:id/:prop').get(getUserProperty);
+router.route('/search').get(searchUser);
+export default router;
