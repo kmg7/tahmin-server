d6e5abcf5537f0901df5f81fa0c96010516a811d
kmlgkcy <gmkemalim@gmail.com>
2023-01-23 16:35:42 +0300

    util: local error messages

---
+++ src/utils/errors/index.js
@@ -0,0 +1,3 @@
+import * as english from './locales/en.js';
+
+export const en = english;
---
+++ src/utils/errors/locales/en.js
@@ -0,0 +1,24 @@
+export const SERVICE = {
+  'string.base': 'Value must be string',
+  'string.empty': 'Value cannot be empty',
+  'string.min': 'Value cannot violate minimum character constraint',
+  'string.max': 'Value cannot violate maximum character constraint',
+  'string.uri': 'Value must be a valid uri',
+  'string.isoDate': 'Value must be in iso format',
+  'number.base': 'Value must be number',
+  'number.integer': 'Value must be integer',
+  'number.min': 'Value cannot violate minimum constraint',
+  'number.max': 'Value cannot violate maximum constraint',
+  'boolean.base': 'Value must be boolean',
+  'any.only': 'Value must be one of allowed values',
+  'any.required': 'Value required',
+  'object.unknown': 'Value/Values not allowed',
+};
+
+export const DATABASE = {
+  P2002: 'Unique constraint violation',
+  P2003: 'Foreign key constraint failed',
+  P2025: 'There are no such record, operation failed',
+};
+
+export const INTERNAL = 'Unexpected internal server error';
