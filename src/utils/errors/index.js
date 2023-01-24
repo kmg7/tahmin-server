8b5706b92ebdd980696e737a76b8024aaa44951f
kmlgkcy <gmkemalim@gmail.com>
2023-01-24 19:25:33 +0300

    local error messages english added

--- src/utils/errors/index.js
+++ src/utils/errors/index.js
@@ -1,3 +1,10 @@
 import * as english from './locales/en.js';
 
+const locales = [{ en_US: english }];
+
+export const getMessage = ({ locale, layer, code }) => {
+  if (locale in locales) return locales[locale][layer][code];
+  return english[layer][code];
+};
+
 export const en = english;
