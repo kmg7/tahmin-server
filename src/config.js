72d21e087bc3a3ad34c16686fcc08af04a1c7ffd
kmlgkcy <gmkemalim@gmail.com>
2023-02-12 13:49:19 +0300

    audit fix, config bug fixed

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const NODE_ENV = process.env.NODE_ENV;
 export const PORT = process.env.PORT || 5000;
 export const HOST = process.env.HOST || 'localhost';
--- src/server.js
+++ src/server.js
@@ -1,20 +1,20 @@
+import { NODE_ENV, PORT, HOST } from './config.js';
 import express from 'express';
-import dotenv from 'dotenv';
 import xss from 'xss-clean';
 import helmet from 'helmet';
 
 import logger from './utils/logger.js';
 import management_app from './management-api/app.js';
+import app from './api/app.js';
 import './utils/authentication/firebase_utils.js';
-import { NODE_ENV, PORT, HOST } from './config.js';
 
-dotenv.config();
 const server = express();
 
 server.set('env', NODE_ENV);
 server.use(xss());
 server.use(helmet());
 server.disable('x-powered-by');
+server.use('/api', app);
 server.use('/management', management_app);
 
 server.get('/', (req, res) => {
